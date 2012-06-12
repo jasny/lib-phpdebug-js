60b0eae55180920dc42b1519992cf509d0cf18d5
Arnold Daniels <arnold@adaniels.nl>
2012-06-12 18:46:06 +0200

    Added index.js file for 'require'

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Package: https://github.com/ajaxorg/lib-phpdebug
+ * 
+ * License: MIT
+ * 
+ * Copyright(c) 2011 Ajax.org B.V. <info AT ajax DOT org>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * Author: Christoph Dorn <christoph@christophdorn.com> (http://www.christophdorn.com/)
+ * 
+ * Purpose of this module:
+ * 
+ *   A [Xdebug](http://www.xdebug.org/) client that connects to Xdebug directly
+ *   (for use on server) or to the debug proxy server (for use in browser).
+ *   
+ *   Provides all the wrapping to make `./dbgp` usable on server and in browser
+ *   and abstracts the [DBGP Protocol](http://www.xdebug.org/docs-dbgp.php) to
+ *   hide low-level connection and session logic.
+ *   
+ */
+
+module.exports = require('./lib/xdebug');
